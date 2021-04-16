39595d0589406be34e07ce158dd8edae1ae88fe1
Ryan Tseng <rtseng99@yahoo.com>
2021-04-16 04:26:42 -0700

    cors error fixed

--- functions/PackageApi.js
+++ functions/PackageApi.js
@@ -3,6 +3,9 @@ const packages = require('../assets/data')
 
 exports.handler = async (event, context, cb) => {
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(packages),
   }
