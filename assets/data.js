15a854e84dd981c4b643c036e1682a8b9b5d8e18
Ryan Tseng <rtseng99@yahoo.com>
2021-04-15 21:33:43 -0700

    API Name Changes

--- assets/data.js
+++ assets/data.js
@@ -1,7 +1,7 @@
 const packages = [
   {
     id: 'recZkNf2kwmdBcqd0',
-    name: 'accent chair',
+    name: 'Hawaii Package',
     price: 25999,
     image:
       'https://dl.airtable.com/.attachmentThumbnails/e8bc3791196535af65f40e36993b9e1f/438bd160',
@@ -14,7 +14,7 @@ const packages = [
   },
   {
     id: 'recEHmzvupvT8ZONH',
-    name: 'albany sectional',
+    name: 'Paris Package',
     price: 109999,
     image:
       'https://dl.airtable.com/.attachmentThumbnails/0be1af59cf889899b5c9abb1e4db38a4/d631ac52',
@@ -22,11 +22,11 @@ const packages = [
     company: 'liddy',
     description:
       'Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge',
-    category: 'living room',
+    category: ' Package',
   },
   {
     id: 'rec5NBwZ5zCD9nfF0',
-    name: 'albany table',
+    name: 'Italy Package',
     price: 309999,
     image:
       'https://dl.airtable.com/.attachmentThumbnails/7478483f40a2f56662a87b304bd4e104/707d397f',
@@ -38,7 +38,7 @@ const packages = [
   },
   {
     id: 'recd1jIVIEChmiwhe',
-    name: 'armchair',
+    name: 'Disneyland Package',
     price: 12599,
     image:
       'https://dl.airtable.com/.attachmentThumbnails/530c07c5ade5acd9934c8dd334458b86/cf91397f',
@@ -51,7 +51,7 @@ const packages = [
   },
   {
     id: 'recoM2MyHJGHLVi5l',
-    name: 'bar stool',
+    name: 'Japan Package',
     price: 4099,
     image:
       'https://dl.airtable.com/.attachmentThumbnails/a6119fabf7256049cc0e8dbcdf536c9c/b0153f66',
@@ -64,7 +64,7 @@ const packages = [
   },
   {
     id: 'recotY5Nh00DQFdkm',
-    name: 'dining table',
+    name: 'Rome Package',
     price: 42999,
     image:
       'https://dl.airtable.com/.attachmentThumbnails/7a38cf782907773d9900165530cfa583/d9f41960',
@@ -77,7 +77,7 @@ const packages = [
   },
   {
     id: 'rec1Ntk7siEEW9ha1',
-    name: 'emperor bed',
+    name: 'New York Package',
     price: 23999,
     image:
       'https://dl.airtable.com/.attachmentThumbnails/0446e84c5bca9643de3452a61b2d6195/1b32f48b',
@@ -90,7 +90,7 @@ const packages = [
   },
   {
     id: 'recNZ0koOqEmilmoz',
-    name: 'entertainment center',
+    name: 'Spain Package',
     price: 59999,
     image:
       'https://dl.airtable.com/.attachmentThumbnails/65708b701baa3a84883ad48301624b44/2de058af',
@@ -104,7 +104,7 @@ const packages = [
   },
   {
     id: 'recrfxv3EwpvJwvjq',
-    name: 'high-back bench',
+    name: 'England Package',
     price: 39999,
     image:
       'https://dl.airtable.com/.attachmentThumbnails/1af97a4d3eb28563962d8e3520727ffc/1b9cc17f',
