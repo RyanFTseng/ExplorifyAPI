8fa6fd471f264d3403c52f5a851269903190ec3a
Ryan Tseng <rtseng99@yahoo.com>
2021-05-04 05:37:14 -0700

    header update

--- functions/products.js
+++ functions/products.js
@@ -45,6 +45,9 @@ exports.handler = async (event, context, cb) => {
       return { id, name, url, price }
     })
     return {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
       statusCode: 200,
       body: JSON.stringify(products),
     }
